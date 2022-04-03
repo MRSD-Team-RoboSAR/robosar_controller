ca9c4bf04544142163265630999948e1dd28ca52
Charvi <charvigupta96@gmail.com>
2022-04-03 13:32:39 -0400

    Changes to client for testing callback

--- src/robosar_controller_client.cpp
+++ src/robosar_controller_client.cpp
@@ -1,15 +1,15 @@
 #include <ros/ros.h>
 #include <actionlib/client/simple_action_client.h>
 #include <actionlib/client/terminal_state.h>
-#include <robosar_controller/PurePursuitAction.h>
+#include <robosar_controller/RobosarControllerAction.h>
 
 int main (int argc, char **argv)
 {
-  ros::init(argc, argv, "test_pure_pursuit");
+  ros::init(argc, argv, "test_controller");
 
   // create the action client
   // true causes the client to spin its own thread
-  actionlib::SimpleActionClient<robosar_controller::PurePursuitAction> ac("controller", true);
+  actionlib::SimpleActionClient<robosar_controller::RobosarControllerAction> ac("controller", true);
 
   ROS_INFO("Waiting for action server to start.");
   // wait for the action server to start
@@ -17,13 +17,29 @@ int main (int argc, char **argv)
 
   ROS_INFO("Action server started, sending goal.");
   // send a goal to the action
-  robosar_controller::PurePursuitGoal goal;
+  robosar_controller::RobosarControllerGoal goal;
 
   goal.path.header.frame_id = "map";
   goal.path.header.stamp = ros::Time::now();
 
   //goal.path.poses[0].pose.position.x = 1.0;
   //goal.path.poses[0].pose.orientation.w = 1.0;
+  geometry_msgs::PoseStamped pose;
+  pose.header.seq=0;
+  pose.header.stamp = ros::Time::now();
+  pose.header.frame_id = "map";
+  pose.pose.position.x = 0.5;
+  pose.pose.position.y = -0.18;
+  pose.pose.orientation.z = -0.18145437944873438;
+  goal.path.poses.push_back(pose);
+
+  pose.header.seq=0;
+  pose.header.stamp = ros::Time::now();
+  pose.header.frame_id = "map";
+  pose.pose.position.x = 0.5;
+  pose.pose.position.y = -0.77;
+  pose.pose.orientation.z = -0.1613906061700738;
+  goal.path.poses.push_back(pose);
   ac.sendGoal(goal);
 
   //wait for the action to return
