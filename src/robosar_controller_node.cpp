fe58c82ab3188e7860241699585e6893a0940695
Neel <neilpatilcoldplay@gmail.com>
2022-04-04 19:24:55 -0400

    Added the lg controller

--- src/robosar_controller_node.cpp
+++ src/robosar_controller_node.cpp
@@ -1,11 +1,11 @@
 // Created by Charvi and Indraneel on 4/04/22
 
-#include "controller_action_server.hpp"
+#include "lg_action_server.hpp"
 
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "robosar_controller");
-  ControllerAction controller("agent0");
+  LGControllerAction controller("agent0");
   ros::spin();
 
   return 0;
