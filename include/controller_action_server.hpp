062edd6fd72f8754a78fecd159d6d57b4f79d9aa
Neel <ipatil@andrew.cmu.edu>
2022-09-05 16:20:26 -0400

    Removed some unecessary header files

--- include/controller_action_server.hpp
+++ include/controller_action_server.hpp
@@ -20,7 +20,6 @@
 #include <geometry_msgs/Vector3Stamped.h>
 #include <nav_msgs/Path.h>
 #include <nav_msgs/Odometry.h>
-#include <ackermann_msgs/AckermannDriveStamped.h>
 
 #include <kdl/frames.hpp>
 #include <robosar_controller/PurePursuitConfig.h>
