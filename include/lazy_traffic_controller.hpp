867339d465b79a3708681a4975e7adf88835ea62
Neel <ipatil@andrew.cmu.edu>
2022-09-20 16:09:38 -0400

    Added subscriber and service to talk to FMS

--- include/lazy_traffic_controller.hpp
+++ include/lazy_traffic_controller.hpp
@@ -9,21 +9,32 @@
 #include <thread>
 #include <ros/console.h>
 #include <ros/ros.h>
+#include <std_msgs/Bool.h>
 
 class LazyTrafficController {
 
 public:
 
-    LazyTrafficController();
+    LazyTrafficController(void);
     ~LazyTrafficController(void);
 
 
 private:
 
+    // typedef Agent {
+    //     string name;
+    // } Agent_s;
+
+    // std::set<Agent_s> agent_set_;
     void RunController(void);
+    void statusCallback(const std_msgs::Bool &status_msg);
 
     std::thread traffic_controller_thread_;
     bool controller_active_;
+    ros::Subscriber status_subscriber_;
+    ros::NodeHandle nh_;
+    bool fleet_status_outdated_;
+    ros::ServiceClient status_client; 
 
 };
 #endif // LAZY_TRAFFIC_CONTROLLER_H
--- src/lazy_traffic_controller.cpp
+++ src/lazy_traffic_controller.cpp
@@ -1,12 +1,21 @@
 // Created by Indraneel and Naren on 19/09/22
 
 #include "lazy_traffic_controller.hpp"
+#include "robosar_messages/agent_status.h"
 
 
 LazyTrafficController::LazyTrafficController(): controller_active_(true) {
     
+    status_subscriber_ = nh_.subscribe("/robosar_agent_bringup_node/status", 1, &LazyTrafficController::statusCallback, this);
+    // Get latest fleet info from agent bringup
+    status_client = nh_.serviceClient<robosar_messages::agent_status>("/robosar_agent_bringup_node/agent_status");
     traffic_controller_thread_ = std::thread(&LazyTrafficController::RunController, this);
 }
+
+void LazyTrafficController::statusCallback(const std_msgs::Bool &status_msg) {
+    fleet_status_outdated_ = true;
+}
+
 
 LazyTrafficController::~LazyTrafficController() {
 
